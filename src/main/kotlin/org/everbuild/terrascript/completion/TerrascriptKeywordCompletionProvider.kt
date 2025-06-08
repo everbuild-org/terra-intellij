2c63dfa9be22486a1fec0bbff4cb7f8143379c05
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-06-08 22:52:34 +0200

    fix: better contextual keyword completion

--- src/main/kotlin/org/everbuild/terrascript/completion/TerrascriptKeywordCompletionProvider.kt
+++ src/main/kotlin/org/everbuild/terrascript/completion/TerrascriptKeywordCompletionProvider.kt
@@ -5,9 +5,11 @@ import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.PsiWhiteSpace
 import com.intellij.util.ProcessingContext
-import org.everbuild.terrascript.psi.impl.TerrascriptStatementImpl
+import org.everbuild.terrascript.psi.TerrascriptExpression
+import org.everbuild.terrascript.psi.TerrascriptFile
+import org.everbuild.terrascript.psi.TerrascriptStatement
 
 class TerrascriptKeywordCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
@@ -18,9 +20,10 @@ class TerrascriptKeywordCompletionProvider : CompletionProvider<CompletionParame
         val position = parameters.position
 
         if (isAtStartOfStatement(position)) {
-            for (kw in KEYWORDS) {
+            for (kw in GENERAL_KEYWORDS) {
                 result.addElement(
-                    LookupElementBuilder.create(kw)
+                    LookupElementBuilder.create(kw + if (kw in SINGLE_LINE_KEYWORDS) ";" else "")
+                        .withPresentableText(kw)
                         .withBoldness(true)
                 )
             }
@@ -28,23 +31,34 @@ class TerrascriptKeywordCompletionProvider : CompletionProvider<CompletionParame
     }
 
     private fun isAtStartOfStatement(position: PsiElement): Boolean {
-        val parent = PsiTreeUtil.getParentOfType<TerrascriptStatementImpl>(position) ?: return true
-        return position.textRange.startOffset == parent.textRange.startOffset
+        var previous = position.prevSibling
+            ?: return position.parent is TerrascriptExpression || position.parent is TerrascriptFile
+        while (previous is PsiWhiteSpace) {
+            previous = previous.prevSibling
+                ?: return position.parent is TerrascriptExpression || position.parent is TerrascriptFile
+        }
+        if (previous.text == ";") return true
+        if (previous is TerrascriptStatement) return true
+        return false
     }
 
     companion object {
-        val KEYWORDS = listOf(
-            "if",
-            "else",
-            "while",
-            "for",
+        // Get autocompleted to <kw>;
+        val SINGLE_LINE_KEYWORDS = listOf(
             "return",
             "fail",
             "break",
             "continue",
+        )
+
+        val GENERAL_KEYWORDS = listOf(
+            "if",
+            "else",
+            "while",
+            "for",
             "str",
             "num",
             "bool"
-        )
+        ) + SINGLE_LINE_KEYWORDS
     }
 }
