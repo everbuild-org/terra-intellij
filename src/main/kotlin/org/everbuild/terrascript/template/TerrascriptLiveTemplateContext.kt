a171e444e3e17c16f1bf6e58872bbaffd816472e
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-06-10 03:29:36 +0200

    fix: context checking in TerrascriptLiveTemplateContext
    Removed unnecessary code and logging from the `isInContext` method to streamline its implementation. This enhances readability and maintains the default behavior of always returning `true`.

--- src/main/kotlin/org/everbuild/terrascript/template/TerrascriptLiveTemplateContext.kt
+++ src/main/kotlin/org/everbuild/terrascript/template/TerrascriptLiveTemplateContext.kt
@@ -2,14 +2,9 @@ package org.everbuild.terrascript.template
 
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
-import org.everbuild.terrascript.completion.TerrascriptKeywordCompletionProvider
-
 
 class TerrascriptLiveTemplateContext : TemplateContextType("Terrascript") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
-        println("asked to check context for")
         return true
-        val currentElement = templateActionContext.file.findElementAt(templateActionContext.startOffset) ?: return true
-        return TerrascriptKeywordCompletionProvider.isAtStartOfStatement(currentElement)
     }
 }
